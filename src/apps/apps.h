20bcc95ceb2edaf9f0d3da6331feeae83f1a5f3d
xiangtailiang <xiangtailiang@foxmail.com>
2025-09-06 10:26:06 +0800

    remove APP_GENERATOR

--- src/apps/apps.h
+++ src/apps/apps.h
@@ -3,8 +3,8 @@
 
 #include "../driver/keyboard.h"
 
-#define APPS_COUNT 17
-#define RUN_APPS_COUNT 11
+#define APPS_COUNT 16
+#define RUN_APPS_COUNT 10
 
 typedef enum {
   APP_NONE,
@@ -22,9 +22,9 @@ typedef enum {
   APP_SETTINGS,
   APP_VFO1,
   APP_VFO2,
-  APP_GENERATOR,
-  APP_ABOUT,
+  // APP_GENERATOR,
   APP_MORSE,
+  APP_ABOUT,
 } AppType_t;
 
 typedef struct App {
