5bf6ec4194fe913dad66b2f4c454d8ed4ca916c4
xiangtailiang <xiangtailiang@foxmail.com>
2025-09-06 10:07:48 +0800

    add APP_GENERATOR

--- src/apps/apps.c
+++ src/apps/apps.c
@@ -89,8 +89,8 @@ const App apps[APPS_COUNT] = {
     {"Settings", NULL, NULL, SETTINGS_render, SETTINGS_key, SETTINGS_deinit},
     {"1 VFO", VFO1_init, VFO1_update, VFO1_render, VFO1_key, NULL},
     {"2 VFO", VFO2_init, VFO2_update, VFO2_render, VFO2_key, NULL},
-    // {"Generator", GENERATOR_init, GENERATOR_update, GENERATOR_render,
-    //  GENERATOR_key, NULL},
+    {"Generator", GENERATOR_init, GENERATOR_update, GENERATOR_render,
+     GENERATOR_key, NULL},
     {"ABOUT", NULL, NULL, ABOUT_Render, ABOUT_key, NULL},
     {"Morse", MORSE_init, MORSE_update, MORSE_render, MORSE_key, MORSE_deinit},
 };
